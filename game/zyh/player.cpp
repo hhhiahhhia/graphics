794fad4b3ac2aa07360490ca7c3c4b2984a00afd
denil1111 <zyh.work@gmail.com>
2015-12-05 17:16:52 +0800

    player.cpp

--- game/zyh/player.cpp
+++ game/zyh/player.cpp
@@ -8,56 +8,38 @@
 
 #include "player.hpp"
 
-extern float fDistance;
 
 void Player::script()
 {
+    double fDistance = 0.1;
     if (keyPushed('a'))
     {
-		if ((camera->location.x-fDistance)>1.0)
-		{
-		  camera->location.x-=fDistance;
-		  camera->center.x-=fDistance;
-		}
+        camera->location.x-=fDistance;
+        camera->center.x-=fDistance;
     }
     if (keyPushed('d'))
     {
-		if ((camera->location.x+fDistance)<99.0)
-		{
-		  camera->location.x+=fDistance;
-		  camera->center.x+=fDistance;
-		}
+        camera->location.x+=fDistance;
+        camera->center.x+=fDistance;
     }
 	if (keyPushed('w'))
 	{
-		if ((camera->location.z-fDistance)>-99.0)
-		{
-		  camera->location.z-=fDistance;
-		  camera->center.z-=fDistance;
-		}
+        camera->location.z-=fDistance;
+        camera->center.z-=fDistance;
 	}
 	if (keyPushed('s'))
 	{
-		if ((camera->location.z+fDistance)<-1.0)
-		{
-		  camera->location.z+=fDistance;
-		  camera->center.z+=fDistance;
-		}
+        camera->location.z+=fDistance;
+        camera->center.z+=fDistance;
 	}
 	if (keyPushed('z'))
 	{
-		if ((camera->location.y+fDistance)<99.0)
-		{
-		  camera->location.y+=fDistance;
-		  camera->center.y+=fDistance;
-		}
+        camera->location.y+=fDistance;
+        camera->center.y+=fDistance;
 	}
 	if (keyPushed('c'))
 	{
-		if ((camera->location.y-fDistance)>1.0)
-		{
-		  camera->location.y-=fDistance;
-		  camera->center.y-=fDistance;
-		}
+        camera->location.y-=fDistance;
+        camera->center.y-=fDistance;
 	}
 }
