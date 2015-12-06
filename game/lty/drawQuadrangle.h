09df73ead0002b6da1579cddc4803db8678b63c4
hhhiahhhia <hhhiahhhia@users.noreply.github.com>
2015-12-07 01:20:02 +0800

    Create drawQuadrangle.h

---
+++ game/lty/drawQuadrangle.h
@@ -0,0 +1,13 @@
+#ifndef drawQuadrangle_h
+#define drawQuadrangle_h
+
+#include <stdio.h>
+#include "../../object.hpp"
+
+class drawQuadrangle:public Object
+{
+public:
+  virtual void draw();
+};
+
+#endif
