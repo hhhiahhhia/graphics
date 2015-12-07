7ee90db9fc2a4bb254d1b975d81c267126ebd7f8
4Frank <fyx325@foxmail.com>
2015-12-07 21:31:39 +0800

    A complete cylinder rolling between walls

--- game/fyx/MyCylinder.h
+++ game/fyx/MyCylinder.h
@@ -6,8 +6,11 @@
 
 class MyCylinder:public Object
 {
+public:
   virtual void script();
   virtual void draw();
+  bool bSign;
+  MyCylinder(){ bSign = true; }
 };
 
 void DrawCircleArea(float cx, float cy, float cz, float r, int num_segments);
