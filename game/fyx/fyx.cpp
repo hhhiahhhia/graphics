b4db9240b550d600726e30cc26c9a1312ef8ce0d
4Frank <fyx325@foxmail.com>
2015-12-07 21:29:27 +0800

    New update

--- game/fyx/fyx.cpp
+++ game/fyx/fyx.cpp
@@ -4,9 +4,11 @@
 void fyxPart(std::vector<Object*>* objectList)
 {
     MyCylinder * C1 = new MyCylinder(); 
-    C1->location.x = 55;
+	C1->bLocationFlag = true;
+    C1->location.x = 60;
     C1->location.y = 3;
     C1->location.z = -3;
-    C1->rotate.y = 90;
+	C1->bRotateFlag = true;
+	C1->rotate.y = 90;
     objectList->push_back(C1);
 }
