2b67d346623db430bf79464ded08bd83a2595e07
denil1111 <zyh.work@gmail.com>
2015-12-08 13:05:29 +0800

    deprecate the flag

--- game/fyx/fyx.cpp
+++ game/fyx/fyx.cpp
@@ -3,12 +3,10 @@
 
 void fyxPart(std::vector<Object*>* objectList)
 {
-    MyCylinder * C1 = new MyCylinder(); 
-	C1->bLocationFlag = true;
+    MyCylinder * C1 = new MyCylinder();
     C1->location.x = 60;
     C1->location.y = 3;
     C1->location.z = -3;
-	C1->bRotateFlag = true;
 	C1->rotate.y = 90;
     objectList->push_back(C1);
 }
