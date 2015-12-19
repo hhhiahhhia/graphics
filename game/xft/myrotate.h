24010b21b50ba6fe6f8382f8086c00b67a234ea3
xtfzzzxxx <445836813@qq.com>
2015-12-19 14:55:35 +0800

    Create myrotate.h

---
+++ game/xft/myrotate.h
@@ -0,0 +1,12 @@
+#ifndef myrotate_h
+#define myrotate_h
+
+#include <stdio.h>
+#include "../../object.hpp"
+
+class myrotate:public Object
+{
+  virtual void script();
+  //virtual void draw();
+};
+#endif
