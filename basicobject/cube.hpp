c8fb4da4d636ab49257e4f37c3cda63acef8073e
denil1111 <zyh.work@gmail.com>
2016-01-04 16:48:43 +0800

    new cube

---
+++ basicobject/cube.hpp
@@ -0,0 +1,19 @@
+//
+//  cube.hpp
+//  mygraphics
+//
+//  Created by zyhc on 1/4/16.
+//  Copyright © 2016 zyhc. All rights reserved.
+//
+
+#ifndef cube_hpp
+#define cube_hpp
+
+#include <stdio.h>
+#include "../object.hpp"
+class Cube:public Object
+{
+public:
+    virtual void draw();
+};
+#endif /* cube_hpp */
