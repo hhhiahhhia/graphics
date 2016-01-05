564ed56e70db41c45563e7a1da0ca5d547d9d80a
4Frank <fyx325@foxmail.com>
2016-01-06 00:54:24 +0800

    Update fyx.cpp

--- game/fyx/fyx.cpp
+++ game/fyx/fyx.cpp
@@ -1,16 +1,68 @@
 #include "fyx.hpp"
 #include "MyCylinder.h"
-#include "Cylinder.h"
+#include "MyObject.h"
+#include "NewWall.h"
+#include "cube.hpp"
+#include "Top.h"
+#include "Bottom.h"
 
 void fyxPart(std::vector<Object*>* objectList)
 {
-	Cylinder *CylinderOne = new Cylinder;
-	objectList->push_back(CylinderOne);
+	MyObject *MyObjectOne = new MyObject;
+	objectList->push_back(MyObjectOne);
 
-    	MyCylinder * CylinderOneOne = new MyCylinder();
+	MyCylinder * CylinderOneOne = new MyCylinder();
     	CylinderOneOne->color = Vector3(0.8,0.9,0.6);
-	CylinderOneOne->location = Vector3(60,3,-3);
+	CylinderOneOne->location = Vector3(60,5,-3);
 	CylinderOneOne->rotate = Vector3(0,90,0);
-	CylinderOne->addChild(CylinderOneOne);
+	MyObjectOne->addChild(CylinderOneOne);
+
+	MyObject *drawModelOneWallOneFrontOne=new MyObject;
+  	drawModelOneWallOneFrontOne->location=Vector3(100.0,0.0,0.0);
+  	MyObjectOne->addChild(drawModelOneWallOneFrontOne);
+  	MyObject *drawModelOneWallOneBackOne=new MyObject;
+  	drawModelOneWallOneBackOne->location=Vector3(0.0,0.0,-100.0);
+  	drawModelOneWallOneBackOne->rotate=Vector3(0.0,180.0,0.0);
+  	MyObjectOne->addChild(drawModelOneWallOneBackOne);
+  	MyObject *drawModelOneWallOneLeftOne=new MyObject;
+  	drawModelOneWallOneLeftOne->rotate=Vector3(0.0,270.0,0.0);
+  	MyObjectOne->addChild(drawModelOneWallOneLeftOne);
+	MyObject *drawModelOneWallOneRightOne = new MyObject;
+  	drawModelOneWallOneRightOne->location=Vector3(100.0,0.0,-100.0);
+  	drawModelOneWallOneRightOne->rotate=Vector3(0.0,90.0,0.0);
+  	MyObjectOne->addChild(drawModelOneWallOneRightOne);
+  	MyObject *drawModelOneWallOneTopOne=new MyObject;
+  	drawModelOneWallOneTopOne->location=Vector3(100.0,100.0,0.0);
+  	drawModelOneWallOneTopOne->rotate=Vector3(270.0,0.0,0.0);
+  	MyObjectOne->addChild(drawModelOneWallOneTopOne);
+  	MyObject *drawModelOneWallOneBottomOne=new MyObject;
+  	drawModelOneWallOneBottomOne->location=Vector3(100.0,0.0,-100.0);
+  	drawModelOneWallOneBottomOne->rotate=Vector3(90.0,0.0,0.0);
+  	MyObjectOne->addChild(drawModelOneWallOneBottomOne);
+
+  	NewWall *drawModelOneWallOne=new NewWall;
+  	drawModelOneWallOne->location=Vector3(-50.0,50.0,0.0);
+  	drawModelOneWallOne->size=Vector3(100.0,100.0,1.0);
+  	drawModelOneWallOneFrontOne->addChild(drawModelOneWallOne);
+  	drawModelOneWallOneBackOne->addChild(drawModelOneWallOne);
+  	drawModelOneWallOneLeftOne->addChild(drawModelOneWallOne);
+  	drawModelOneWallOneRightOne->addChild(drawModelOneWallOne);
+  	Cube *drawModelOneWallOneQuadrangleOne=new Cube;
+	drawModelOneWallOneQuadrangleOne->rotate = Vector3(0.0,0.0,90.0);
+  	drawModelOneWallOne->addChild(drawModelOneWallOneQuadrangleOne);
+
+	Top *drawModelOneWallTwo=new Top;
+  	drawModelOneWallTwo->location=Vector3(-50.0,50.0,0.0);
+  	drawModelOneWallTwo->size=Vector3(100.0,100.0,1.0);
+  	drawModelOneWallOneTopOne->addChild(drawModelOneWallTwo);
+  	Cube * drawModelOneWallOneQuadrangleTwo=new Cube;
+  	drawModelOneWallTwo->addChild(drawModelOneWallOneQuadrangleTwo);
+
+  	Bottom *drawModelOneWallThree=new Bottom;
+  	drawModelOneWallThree->location=Vector3(-50.0,50.0,0.0);
+  	drawModelOneWallThree->size=Vector3(100.0,100.0,1.0);
+  	drawModelOneWallOneBottomOne->addChild(drawModelOneWallThree);
+  	Cube * drawModelOneWallOneQuadrangleThree=new Cube;
+  	drawModelOneWallThree->addChild(drawModelOneWallOneQuadrangleThree);
 }
 
