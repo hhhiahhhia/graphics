8abbf1d9facdc4631b155c5dad3b7f84a5457b01
4Frank <fyx325@foxmail.com>
2016-01-09 16:57:33 +0800

    Update fyx.cpp

--- game/fyx/fyx.cpp
+++ game/fyx/fyx.cpp
@@ -4,14 +4,17 @@
 #include "NewWall.h"
 #include "Top.h"
 #include "Bottom.h"
+#include "../../basicobject/cube.hpp"
+#include "Door.h"
+#include "Out.h"
 
 void fyxPart(std::vector<Object*>* objectList)
 {
 	MyObject *MyObjectOne = new MyObject;
 	objectList->push_back(MyObjectOne);
 
-	MyCylinder * CylinderOne = new MyCylinder();
-	CylinderOne->color = Vector3(0.8,0.9,0.6);
+    MyCylinder * CylinderOne = new MyCylinder();
+    CylinderOne->color = Vector3(0.8,0.9,0.6);
 	CylinderOne->location = Vector3(60,5,-3);
 	CylinderOne->rotate = Vector3(0,90,0);
 	MyObjectOne->addChild(CylinderOne);
@@ -41,7 +44,7 @@ void fyxPart(std::vector<Object*>* objectList)
 
   	NewWall *AllWall = new NewWall;
   	AllWall->location = Vector3(-50.0,50.0,0.0);
-  	AllWall->size = Vector3(100.0,100.0,1.0);
+  	AllWall->size = Vector3(100.0,100.0,2.0);
   	FrontWall->addChild(AllWall);
   	BackWall->addChild(AllWall);
   	LeftWall->addChild(AllWall);
@@ -57,5 +60,33 @@ void fyxPart(std::vector<Object*>* objectList)
   	Bottomt->size = Vector3(100.0,100.0,1.0);
   	BottomWall->addChild(Bottomt);
 
+  	MyObject *CompleteDoor = new MyObject;
+  	CompleteDoor->location = Vector3(50.0,10.0,-98.4);
+  	MyObjectOne->addChild(CompleteDoor);
+  	Door *MyDoor = new Door;
+  	MyDoor->location = Vector3(0.0,0.0,0.0);
+  	MyDoor->size = Vector3(12.0,20.0,1.0);
+  	CompleteDoor->addChild(MyDoor);
+  	Out *DoorOut = new Out;
+  	DoorOut->color = Vector3(0.0,0.0,0.0);
+  	DoorOut->location = Vector3(0.0,0.0,-1.0);
+  	DoorOut->size = Vector3(12.0,20.0,1.0);
+  	CompleteDoor->addChild(DoorOut);
+  	Cube *Doorbar1 = new Cube;
+  	Doorbar1->color = Vector3(0.501,0.2,0);
+  	Doorbar1->location = Vector3(-6.0,0.0,0.0);
+  	Doorbar1->size = Vector3(1.0,21.0,2.0);
+  	CompleteDoor->addChild(Doorbar1);
+  	Cube *Doorbar2 = new Cube;
+  	Doorbar2->color = Vector3(0.501,0.2,0);
+  	Doorbar2->location = Vector3(6.0,0.0,0.0);
+  	Doorbar2->size = Vector3(1.0,21.0,2.0);
+  	CompleteDoor->addChild(Doorbar2);
+  	Cube *Doorbar3 = new Cube;
+  	Doorbar3->color = Vector3(0.501,0.2,0);
+  	Doorbar3->location = Vector3(0.0,10.0,0.0);
+  	Doorbar3->size = Vector3(12.0,1.0,2.0);
+  	CompleteDoor->addChild(Doorbar3);
 }
+
 
